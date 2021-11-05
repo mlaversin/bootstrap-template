4d049e76b19adb06f9bb00e09968d5131ebdf402
Morgan Laversin <morganlaversin@gmail.com>
2021-11-05 09:34:40 +0100

    gallery section

---
+++ app.js
@@ -0,0 +1,16 @@
+// Gallery section - Expanding cards
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
